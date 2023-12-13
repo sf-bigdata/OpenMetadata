973df58b6b29a6103fe6cc6dcdc443420012b42d
Alex Liu <alex.liu05@sap.com>
2023-12-13 15:48:06 +0800

    Update

--- env39/lib/python3.10/site-packages/sqlfluff/core/plugin/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Marker to be imported and used in plugins (and for own implementations)."""
-import pluggy
-
-project_name = "sqlfluff"
-plugin_base_name = f"{project_name}-plugin"
-hookimpl = pluggy.HookimplMarker(plugin_base_name)
